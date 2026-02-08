9fbf7ef0203839cc9e36731f0ca0f53efda78ea8
Lohith R <156739882+Lohithravi69@users.noreply.github.com>
2026-02-08 22:39:53 +0530

    Add files via upload

---
+++ java/Problems/merge_arr.java
@@ -0,0 +1,16 @@
+package Problems;
+
+public class merge_arr {
+	
+	Boolean merge_array() {
+		
+		return(Boolean)null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+	

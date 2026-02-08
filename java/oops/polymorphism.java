f56b4f471761c7fcf3f0188aaf961024eb52d8a0
Lohith R <156739882+Lohithravi69@users.noreply.github.com>
2026-02-08 22:42:53 +0530

    Add files via upload

---
+++ java/oops/polymorphism.java
@@ -0,0 +1,16 @@
+package oops;
+public class polymorphism {
+	public static void main(String args[]) {
+		System.out.println(poly());
+	}
+	static int poly() {
+		 int num=1;
+		 System.out.println( sec());
+	return num;
+		
+ }
+	static Double sec() {
+		double num2 =2.33;
+		return (double) num2;
+	}
+}

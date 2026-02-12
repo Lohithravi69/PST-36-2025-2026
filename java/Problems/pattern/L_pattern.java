352659ab3cd25271fa97e7d632bfb679784d440d
Lohith R <156739882+Lohithravi69@users.noreply.github.com>
2026-02-12 20:50:53 +0530

    Add files via upload

---
+++ java/Problems/pattern/L_pattern.java
@@ -0,0 +1,24 @@
+package package1;
+import java.util.*;
+public class L_pattern {
+
+	public static void main(String[] args) {
+		
+		L_pat.method();
+	}
+
+}
+class L_pat{
+	static void method() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a size of pattern");
+		int size=sc.nextInt();
+		for(int i=0;i<size-1;i++) {
+			System.out.println("*");}
+		System.out.print("*");
+			for(int j=0;j<size-1;j++) {
+				System.out.print(" *");
+			}
+		
+	}
+}

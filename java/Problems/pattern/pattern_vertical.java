6e5bcc3f8be65133d11d3e795050e60b175444bc
Lohith R <156739882+Lohithravi69@users.noreply.github.com>
2026-02-12 20:48:48 +0530

    Add files via upload

---
+++ java/Problems/pattern/pattern_vertical.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class pattern_vertical {
+	public static void main(String args[]) {
+		System.out.println(vertical());
+	}
+	 static String vertical() {
+		 
+		for(int i=0;i<9;i++) {
+			System.out.println("*");
+		}
+		return "*";
+		
+	}
+	
+
+}

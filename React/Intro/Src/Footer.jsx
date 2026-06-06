ad0b7b890a52de220d4bee2fee81a45443d33d11
Lohith R <156739882+Lohithravi69@users.noreply.github.com>
2026-06-06 11:43:41 +0530

    Add files via upload

---
+++ React/Intro/Src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy;{new Date().getFullYear()} my website name</p>
+        </footer>
+    )
+
+    
+}
+export default Footer;
